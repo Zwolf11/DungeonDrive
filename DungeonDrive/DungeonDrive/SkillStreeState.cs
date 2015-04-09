170458b579af2e264204f02677e6fd65d013055e
jiang <jiang46@purdue.edu>
2015-04-09 15:06:11 -0400

    empty skillstree

---
+++ DungeonDrive/DungeonDrive/SkillStreeState.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace DungeonDrive
+{
+    class SkillStreeState : State
+    {
+
+        public SkillStreeState(MainForm form) : base(form) { }
+        private GameState state { get { return (GameState)parent; } }
+        public override void keyDown(object sender, KeyEventArgs e) { 
+        }
+        public override void mouseMove(object sender, MouseEventArgs e) { 
+        }
+
+        public override void mouseDown(object sender, MouseEventArgs e) { 
+        }
+        public override void mouseUp(object sender, MouseEventArgs e) { 
+        }
+        public override void tick(object sender, EventArgs e) { 
+        }
+        public override void keyUp(object sender, KeyEventArgs e) { 
+        }
+        public override void paint(object sender, PaintEventArgs e) { 
+        }
+    }
+}
